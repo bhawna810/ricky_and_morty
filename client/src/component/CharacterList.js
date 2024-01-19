28bd125581c6c1ab861b0f6528365d5f5516dc59
bhawna810 <bhawna842000@gmail.com>
2024-01-20 05:10:32 +0530

    modify charaterList and server

--- client/src/component/CharacterList.js
+++ client/src/component/CharacterList.js
@@ -1,81 +1,166 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import ProductCard from './ProductCard';
+// import { Container , Row, Col} from 'reactstrap';
+
+// import "../styles/character-list.css";
+
+// const CharacterList = () => {
+//   const [characters, setCharacters] = useState([]);
+
+//   useEffect(() => {
+//     const fetchCharacters = async () => {
+//       const response = await axios.get('https://ricky-and-morty-backend.onrender.com/characters');
+//       setCharacters(response.data);
+//     };
+
+//     fetchCharacters();
+//   }, []);
+
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [productPerPage] = useState(4);
+//   const [numberOfButton] = useState(4);
+
+//   const indexOfLastProduct = currentPage * productPerPage;
+//   const indexOfFirstProduct = indexOfLastProduct - productPerPage;
+//   const currentProduct = characters.slice(indexOfFirstProduct, indexOfLastProduct);
+//   const totalPages = Math.ceil(characters.length / productPerPage);
+
+//   const handleClick = (pageNumber) => {
+//     setCurrentPage(pageNumber);
+//   };
+
+//   function renderPageNumbers() {
+//     const pageNumber = [];
+
+//     let startIndex = Math.max(1, currentPage - 1);
+//     let endIndex = Math.min(startIndex + numberOfButton - 1, totalPages);
+
+//     if (startIndex + numberOfButton - 1 >= totalPages) {
+//       startIndex = totalPages - numberOfButton + 1;
+//     }
+
+//     for (let i = startIndex; i <= endIndex; i++) {
+//       pageNumber.push(
+//         <button onClick={() => handleClick(i)} disabled={currentPage === i} className='pagination_button'>
+//           {i}
+//         </button>
+//       );
+//     }
+//     return pageNumber;
+//   }
+
+//   return (
+//     <section className='characterlist'>
+//       <Container>
+//         <Row>
+//              <h3 className='heading  mt-4'> Welcome to our Application</h3>
+//               {currentProduct.map((character, index) => 
+//                   <Col lg='3' md='4' sm='6' xs='12' >
+//                      <Link to={`/character/${character.id}`} className='text-decoration-none'>
+//                        <div className='col_div'>
+//                           <ProductCard items ={character} key = {index}/>
+//                        </div>
+                        
+//                     </Link>
+//                   </Col>
+//                 )}
+//         </Row>
+//         <Row>
+//           <div className='pagination_div '>
+//               {renderPageNumbers()}
+//           </div>
+//         </Row>
+//       </Container>
+//     </section>
+   
+//   );
+// }
+
+// export default CharacterList;
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import ProductCard from './ProductCard';
-import { Container , Row, Col} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import "../styles/character-list.css";
 
 const CharacterList = () => {
-  const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [limit] = useState(7);
 
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      const response = await axios.get('https://ricky-and-morty-backend.onrender.com/characters');
-      setCharacters(response.data);
+    useEffect(() => {
+        getPaginatedCharacters();
+    }, [currentPage]);
+
+    const getPaginatedCharacters = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/characters?page=${currentPage}&limit=${limit}`);
+            setCharacters(response.data.characters);
+            setTotalPages(response.data.totalPages);
+
+            console.log(response.data.characters);
+        } catch (error) {
+            console.error('Error fetching characters:', error);
+        }
     };
 
-    fetchCharacters();
-  }, []);
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [productPerPage] = useState(4);
-  const [numberOfButton] = useState(4);
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
 
-  const indexOfLastProduct = currentPage * productPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productPerPage;
-  const currentProduct = characters.slice(indexOfFirstProduct, indexOfLastProduct);
-  const totalPages = Math.ceil(characters.length / productPerPage);
-
-  const handleClick = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  function renderPageNumbers() {
-    const pageNumber = [];
-
-    let startIndex = Math.max(1, currentPage - 1);
-    let endIndex = Math.min(startIndex + numberOfButton - 1, totalPages);
-
-    if (startIndex + numberOfButton - 1 >= totalPages) {
-      startIndex = totalPages - numberOfButton + 1;
-    }
-
-    for (let i = startIndex; i <= endIndex; i++) {
-      pageNumber.push(
-        <button onClick={() => handleClick(i)} disabled={currentPage === i} className='pagination_button'>
-          {i}
-        </button>
-      );
-    }
-    return pageNumber;
-  }
-
-  return (
-    <section className='characterlist'>
-      <Container>
-        <Row>
-             <h3 className='heading  mt-4'> Welcome to our Application</h3>
-              {currentProduct.map((character, index) => 
-                  <Col lg='3' md='4' sm='6' xs='12' >
-                     <Link to={`/character/${character.id}`} className='text-decoration-none'>
-                       <div className='col_div'>
-                          <ProductCard items ={character} key = {index}/>
-                       </div>
-                        
-                    </Link>
-                  </Col>
-                )}
-        </Row>
-        <Row>
-          <div className='pagination_div '>
-              {renderPageNumbers()}
-          </div>
-        </Row>
-      </Container>
-    </section>
-   
-  );
-}
+    return (
+        <section className='characterlist'>
+            <Container>
+                <Row>
+                    <h3 className='heading mt-4'> Welcome to our Application</h3>
+                    {characters?.map((character, index) => (
+                        <Col lg='3' md='4' sm='6' xs='12' key={index}>
+                            <Link to={`/character/${character.id}`} className='text-decoration-none'>
+                                <div className='col_div'>
+                                    <ProductCard items={character} />
+                                </div>
+                            </Link>
+                        </Col>
+                    ))}
+                </Row>
+                <Row>
+                    <div className='pagination_div'>
+                        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+                            Previous
+                        </button>
+                        {[...Array(totalPages).keys()].map((pageNumber) => (
+                            <button
+                                key={pageNumber + 1}
+                                onClick={() => setCurrentPage(pageNumber + 1)}
+                                disabled={currentPage === pageNumber + 1}
+                            >
+                                {pageNumber + 1}
+                            </button>
+                        ))}
+                        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                            Next
+                        </button>
+                    </div>
+                </Row>
+            </Container>
+        </section>
+    );
+};
 
 export default CharacterList;
--- server/index.js
+++ server/index.js
@@ -9,9 +9,27 @@ app.use(cors());
 
 app.get('/characters', async (req, res) => {
     try {
-        const response = await axios.get('https://rickandmortyapi.com/api/character/');
-        res.json(response.data.results);
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit) || 7;
+
+        const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}&limit=${limit}`);
+        const characters = response.data.results.map(character => ({
+            id: character.id,
+            name: character.name,
+            image: character.image,
+            species: character.species,
+            status: character.status,
+            origin: character.origin.name,
+            location: character.location.name
+        }));
+
+        res.json({
+            characters,
+            page,
+            totalPages: Math.ceil(response.data.info.count / limit)
+        });
     } catch (error) {
+        console.error('Error fetching characters:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
@@ -20,7 +38,16 @@ app.get('/character/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
-        res.json(response.data);
+        const character = {
+            id: response.data.id,
+            name: response.data.name,
+            image: response.data.image,
+            species: response.data.species,
+            status: response.data.status,
+            origin: response.data.origin.name,
+            location: response.data.location.name
+        };
+        res.json(character);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
